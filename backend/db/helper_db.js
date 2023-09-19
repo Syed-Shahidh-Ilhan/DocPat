2456fb1940881a7925fa92aa50f3176f47c293c2
Akshitha R <akshithar@Akshithas-MacBook-Pro.local>
2023-09-19 21:57:03 +0530

    basic crud

--- backend/db/helper_db.js
+++
@@ -1,7 +0,0 @@
-import Doctor from "../models/Doctor.js";
-import Patient from "../models/Patient.js";
-import Appointment from "../models/Appointment.js";
-import "./mongoose.js";
-
-await Appointment.deleteMany({})
-console.log("completed")
